ce94c23f0cc798acf8fa5cef2be9651cbd3bbdf3
NiteshHalai <nitesh.halai@gmail.com>
2022-11-11 22:19:55 +0300

    Update shell.c

--- shell.c
+++ shell.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void kash_exit() 
+void exit() 
 {
     exit(0);
 }
 
-char* kash_read_line() {
+char* read_line() {
     char *line = NULL;
     size_t buflen = 0;
     getline(&line, &buflen, stdin);
@@ -17,7 +17,7 @@ char* kash_read_line() {
 #include <sys/wait.h>
 #include <unistd.h>
 
-void kash_exec(char **args) {
+void exec(char **args) {
     pid_t child_pid = fork();
 
     if (child_pid == 0) {
@@ -36,7 +36,7 @@ void kash_exec(char **args) {
 
 #include <string.h>
 
-char** kash_split_line(char *line) {
+char** split_line(char *line) {
     int length = 0;
     int capacity = 16;
     char **tokens = malloc(capacity * sizeof(char*));
@@ -68,11 +68,11 @@ int main()
 
     while (true) {
         printf("> ");
-        line = kash_read_line();
-        tokens = kash_split_line(line);
+        line = read_line();
+        tokens = split_line(line);
 
         if (tokens[0] != NULL) {
-            kash_exec(tokens);
+            exec(tokens);
         }
 
         free(tokens);
