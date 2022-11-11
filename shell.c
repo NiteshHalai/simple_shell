7061dadd18b874fbe127f4a6e9049ad8b2773dbf
NiteshHalai <nitesh.halai@gmail.com>
2022-11-11 22:52:33 +0300

    Update shell.c

--- shell.c
+++ shell.c
@@ -7,26 +7,6 @@
 #include <unistd.h>
 #include <string.h>
 
-void exec(char **args) {
-    pid_t child_pid = fork();
-
-    if (child_pid == 0) {
-        execvp(args[0], args);
-        perror("kash");
-        exit(1);
-    } else if (child_pid > 0) {
-        int status;
-        do {
-            waitpid(child_pid, &status, WUNTRACED);
-        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-    } else {
-        perror("kash");
-    }
-}
-
-
-
-
 
 int main() 
 {
