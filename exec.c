265575e20a7fd91f05acc55a4cd643f05de0224d
NiteshHalai <nitesh.halai@gmail.com>
2022-11-11 22:52:14 +0300

    Create exec.c

---
+++ exec.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/wait.h>
+#include <unistd.h>
+#include <string.h>
+
+void exec(char **args) {
+    pid_t child_pid = fork();
+
+    if (child_pid == 0) {
+        execvp(args[0], args);
+        perror("kash");
+        exit(1);
+    } else if (child_pid > 0) {
+        int status;
+        do {
+            waitpid(child_pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    } else {
+        perror("kash");
+    }
+}
