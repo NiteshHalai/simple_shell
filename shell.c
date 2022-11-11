9e61d34770a266f6a77f6dc146649951400c63bb
NiteshHalai <nitesh.halai@gmail.com>
2022-11-11 22:49:53 +0300

    Update shell.c

--- shell.c
+++ shell.c
@@ -5,6 +5,7 @@
 
 #include <sys/wait.h>
 #include <unistd.h>
+#include <string.h>
 
 void exec(char **args) {
     pid_t child_pid = fork();
@@ -23,31 +24,9 @@ void exec(char **args) {
     }
 }
 
-#include <string.h>
 
-char** split_line(char *line) {
-    int length = 0;
-    int capacity = 16;
-    char **tokens = malloc(capacity * sizeof(char*));
 
-    char *delimiters = " \t\r\n";
-    char *token = strtok(line, delimiters);
 
-    while (token != NULL) {
-        tokens[length] = token;
-        length++;
-
-        if (length >= capacity) {
-            capacity = (int) (capacity * 1.5);
-            tokens = realloc(tokens, capacity * sizeof(char*));
-        }
-
-        token = strtok(NULL, delimiters);
-    }
-
-    tokens[length] = NULL;
-    return tokens;
-}
 
 int main() 
 {
