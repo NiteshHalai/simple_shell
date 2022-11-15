52e09e5da2e80e9a4b40e3ff3a97193e9811d18e
NiteshHalai <nitesh.halai@gmail.com>
2022-11-15 21:39:06 +0300

    Update main.h

--- main.h
+++ main.h
@@ -6,6 +6,20 @@
 #include <stdarg.h>
 #include <math.h>  
 
+struct builtin {
+    char *name;
+    void (*func)(char **args);
+};
+
+struct builtin builtins[] = {
+    {"exit", kash_exit},
+    {"cd", kash_cd},
+};
+
+int kash_num_builtins() {
+    return sizeof(builtins) / sizeof(struct builtin);
+}
+
 int new_putchar(char);
 char* read_line();
 void exec(char **args);
@@ -14,3 +28,4 @@ char *lsh_read_line(void);
 char* my_strtok(char* s, char* delm);
 
 #endif
+
