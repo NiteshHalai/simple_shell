7df017e6aa5f85df5d8a1f92255b72fc61cdb476
NiteshHalai <nitesh.halai@gmail.com>
2022-11-15 21:45:16 +0300

    Update exit.c

--- exit.c
+++ exit.c
@@ -1,3 +1,12 @@
+#include "main.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/wait.h>
+#include <unistd.h>
+#include <string.h>
+
 void kash_exit(char **args) {
     exit(0);
 }
