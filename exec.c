af331a28d902083e75a7fbe16ce5efbdf86e0787
NiteshHalai <nitesh.halai@gmail.com>
2022-11-15 22:17:30 +0300

    Update exec.c

--- exec.c
+++ exec.c
@@ -7,6 +7,20 @@
 #include <unistd.h>
 #include <string.h>
 
+struct builtin {
+    char *name;
+    void (*func)(char **args);
+};
+
+struct builtin builtins[] = {
+    {"exit", kash_exit},
+    {"cd", kash_cd},
+};
+
+int kash_num_builtins() {
+    return sizeof(builtins) / sizeof(struct builtin);
+}
+
 void exec(char **args) {
     
     pid_t child_pid = fork();
