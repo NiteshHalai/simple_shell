97d5d8738b97fec256c584ba60a48fda16a6ade8
NiteshHalai <nitesh.halai@gmail.com>
2022-11-15 21:45:36 +0300

    Update main.h

--- main.h
+++ main.h
@@ -26,6 +26,8 @@ void exec(char **args);
 char** split_line(char *line);
 char *lsh_read_line(void);
 char* my_strtok(char* s, char* delm);
+void kash_cd(char **args);
+void kash_exit(char **args);
 
 #endif
 
